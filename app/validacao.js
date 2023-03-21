90c3e2b8001a9f49f3c3fc96875f09e5c21355bc
oc-garcia <octaviogarcia1988@gmail.com>
2023-03-21 18:39:36 -0300

    Bug fix nas dicas

--- app/validacao.js
+++ app/validacao.js
@@ -12,11 +12,15 @@ function verificaValorValido(chute) {
   }
 
   if (numero === numeroSecreto) {
-    document.body.innerHTML = `<h1>Você acertou!</h1><h3>O numero secreto era ${numeroSecreto}</h3>`;
+    document.body.innerHTML = `
+    <h1>Você acertou!</h1>
+    <h3>O numero secreto era ${numeroSecreto}</h3>
+    `;
   } else if (numero > numeroSecreto) {
-    elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-${down}-long"></i></div>`;
+    elementoChute.innerHTML += `
+    <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`;
   } else {
-     elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-${up}-long"></i></div>`;
+     elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`;
   }
 }
 
