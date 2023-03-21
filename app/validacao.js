c2541a0eaf1c50e47467084bc09824c1d13821da
oc-garcia <octaviogarcia1988@gmail.com>
2023-03-21 18:45:37 -0300

    Incluindo botão jogar novamente

--- app/validacao.js
+++ app/validacao.js
@@ -15,12 +15,14 @@ function verificaValorValido(chute) {
     document.body.innerHTML = `
     <h1>Você acertou!</h1>
     <h3>O numero secreto era ${numeroSecreto}</h3>
+
+    <button id="jogar-novamente" class"btn-jogar">Jogar Novamente</button>
     `;
   } else if (numero > numeroSecreto) {
     elementoChute.innerHTML += `
     <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`;
   } else {
-     elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`;
+    elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`;
   }
 }
 
@@ -31,3 +33,11 @@ function chuteInvalido(numero) {
 function numeroMaiorOuMenor(numero) {
   return numero > maiorValor || numero < menorValor;
 }
+
+document.body.addEventListener("click", (e) => {
+  if (e.target.id == "jogar-novamente") {
+    Window.location.reload();
+  }
+});
+
+function name(params) {}
