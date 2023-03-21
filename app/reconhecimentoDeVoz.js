352caf6934c984a946895d21769b25a6e59215a3
oc-garcia <octaviogarcia1988@gmail.com>
2023-03-21 18:27:43 -0300

    Logica do jogo, acerto, erros e dicas.

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -12,6 +12,7 @@ recognition.addEventListener("result", onSpeak);
 function onSpeak(e) {
   chute = e.results[0][0].transcript;
   exibeChuteNaTela(chute);
+  verificaValorValido(chute);
 }
 
 function exibeChuteNaTela(chute) {
@@ -20,3 +21,5 @@ function exibeChuteNaTela(chute) {
     <span class="box">${chute}</span>
     `;
 }
+
+recognition.addEventListener("end", () => recognition.start());
---
+++ app/validacao.js
@@ -0,0 +1,29 @@
+function verificaValorValido(chute) {
+  const numero = +chute;
+
+  if (chuteInvalido(numero)) {
+    elementoChute.innerHTML += "<div>Valor invalido: não é um número</div>";
+    return;
+  }
+
+  if (numeroMaiorOuMenor(numero)) {
+    elementoChute.innerHTML += `<div>Valor invalido: fale um número entre ${menorValor} e ${maiorValor}</div>`;
+    return;
+  }
+
+  if (numero === numeroSecreto) {
+    document.body.innerHTML = `<h1>Você acertou!</h1><h3>O numero secreto era ${numeroSecreto}</h3>`;
+  } else if (numero > numeroSecreto) {
+    elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-${down}-long"></i></div>`;
+  } else {
+     elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-${up}-long"></i></div>`;
+  }
+}
+
+function chuteInvalido(numero) {
+  return Number.isNaN(numero);
+}
+
+function numeroMaiorOuMenor(numero) {
+  return numero > maiorValor || numero < menorValor;
+}
